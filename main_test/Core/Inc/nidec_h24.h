57b609e0f5d57f958e8c69eb5bc9c9337a503e6d
simo1101 <152496154+simo1101@users.noreply.github.com>
2024-09-11 13:57:53 +0200

    Update nidec_h24.h

--- main_test/Core/Inc/nidec_h24.h
+++ main_test/Core/Inc/nidec_h24.h
@@ -1,13 +1,13 @@
 #include "stm32f4xx_hal.h"
 
+/* To initialize the PWM and direction pins of the motor */
 void nidec_h24_init(uint16_t DIRECTION_Pin, GPIO_TypeDef *DIRECTION_GPIO_Port);
 
+/* To make the motor turn based on the input */
 void nidec_h24_Move(int32_t dutyCycle);
 
-uint32_t nidec_h24_getCnt(TIM_HandleTypeDef *htim);
-uint32_t nidec_h24_getArr(TIM_HandleTypeDef *htim);
-uint32_t nidec_h24_getCrr(TIM_HandleTypeDef *htim);
-
-uint32_t get_rate_cha();
-uint32_t get_rate_chb();
-
+/* Useful getter functions to access the parameters of the PWM signal */
+uint32_t nidec_h24_GetPeriod();
+uint32_t nidec_h24_GetPrescaler();
+uint32_t nidec_h24_GetCounter();
+uint32_t nidec_h24_GetDutyCycle();
