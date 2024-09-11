a2d0327b97261061b6a8bec9815cc43ec2568703
simo1101 <152496154+simo1101@users.noreply.github.com>
2024-09-11 13:52:56 +0200

    Update pid.h

--- main_test/Core/Inc/pid.h
+++ main_test/Core/Inc/pid.h
@@ -22,10 +22,15 @@ typedef struct {
 } PID_t;
 
 void pid_init(PID_t *p, float k_p, float k_i, float k_d, float min_out, float max_out);
-void pid_set_setpoint(PID_t *p, float set_point);
-float pid_get_setpoint(PID_t *p);
+
 float pid_compute_control_action(PID_t *p, float est_output, float *est_out_derivative);
-float pid_get_error(PID_t *p);
 void pid_update_ts(PID_t *p);
+
+/* Useful setter functions */
+void pid_set_setpoint(PID_t *p, float set_point);
+
+/* Useful getter functions */
+float pid_get_setpoint(PID_t *p);
+float pid_get_error(PID_t *p);
 
 #endif
