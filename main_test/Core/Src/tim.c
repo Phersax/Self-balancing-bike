1833b36e55935294af0719abd65a7ae3b3ab6662
Francesco8501 <stasi.fr0108@gmail.com>
2024-09-20 16:54:27 +0200

    Maximizing pwm resolution (PSC and ARR setting)

--- main_test/Core/Src/tim.c
+++ main_test/Core/Src/tim.c
@@ -82,9 +82,9 @@ void MX_TIM5_Init(void)
 
   /* USER CODE END TIM5_Init 1 */
   htim5.Instance = TIM5;
-  htim5.Init.Prescaler = 43;
+  htim5.Init.Prescaler = 0;
   htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim5.Init.Period = 99;
+  htim5.Init.Period = 3999;
   htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim5.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_PWM_Init(&htim5) != HAL_OK)
