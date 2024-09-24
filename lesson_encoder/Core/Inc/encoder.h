a5d3566747e4a25b91a6403c6371e3b531cf0c00
simo1101 <152496154+simo1101@users.noreply.github.com>
2024-09-24 18:53:39 +0200

    Encoder testing

---
+++ lesson_encoder/Core/Inc/encoder.h
@@ -0,0 +1,28 @@
+#ifndef ENCODER_INC_ENCODER_H_
+#define ENCODER_INC_ENCODER_H_
+
+#include "main.h"
+
+typedef enum {
+    ENCODER_RES_2 = 2,
+    ENCODER_RES_4 = 4
+} encoder_resolution_t;
+
+typedef struct {
+    TIM_HandleTypeDef *tim;
+    uint32_t last_sampling_t;
+    uint32_t last_count;
+    uint32_t ppr;
+    encoder_resolution_t resolution;
+    float velocity_pps;
+
+} encoder_t;
+
+HAL_StatusTypeDef encoder_init(encoder_t *encoder, TIM_HandleTypeDef *tim, uint32_t ppr, encoder_resolution_t resolution);
+
+float encoder_get_velocity_rps(encoder_t *encoder);
+float encoder_get_velocity_rpm(encoder_t *encoder);
+float encoder_get_velocity_rads(encoder_t *encoder);
+
+flaot encoder_get_position_deg(encoder_t *encoder);
+flaot encoder_get_position_rads(encoder_t *encoder);
---
+++ lesson_encoder/Core/Src/encoder.c
@@ -0,0 +1,73 @@
+#include "encoder.h"
+#inlude "main.h"
+#include <math.h>
+
+uint32_t cur_cnt;
+
+HAL_StatusTypeDef encoder_init(encoder_t *e, TIM_HandleTypeDef *htim)
+{
+    HAL_StatusTypeDeF ret;
+    ret= HAL_OK;
+    if(tim == NULL){
+        return HAL_ERROR;
+    }
+
+    e -> tim = htim;
+    ret= HAL_ADC_Encoder_Start(e -> tim, TIM_CHANNEL_ALL);
+    if(ret != HAL_OK){
+        return ret;
+    }
+}
+
+ static void __encoder_update(encoder_t *e)
+{
+    uint32_t now;
+    int32_t diff;
+    float cur_velocity, dt;
+
+    now = HAL_GetTick();
+    cur_cnt = e -> tim -> Instance -> CNT;
+
+    diff = (int) cur_cnt - (int) e -> last_count;
+    dt = (float) (now - e -> last_sampling_t) / 1000.0;
+    
+    cur_velocity = (float) diff / dt / (float) e -> resolution;
+
+    e -> velocity_pps = 0.95 * e -> velocity_pps + 0.05 * cur_velocity;
+    
+    e -> last_count = 0;
+    e -> last_sampling_t = now;
+    e -> tim -> Instance -> CNT = 0;
+}
+
+float encoder_get_velocity_rps(encoder_t *e)
+{
+    __encoder_update(e);
+    return e -> velocity_pps / (float) e -> ppr;
+}
+
+
+float encooder_get_velocity_rpm(encoder_t *e)
+{
+    __encoder_update(e);
+    return (e->velocity_pps / (float) e->ppr) * 60.0;
+}
+
+
+float encoder_get_velocity_rads(encoder_t *e)
+{
+    __encoder_update(e);
+    return e->velocity_pps / (float) e -> ppr / (2.0*M_PI);
+}
+
+float encoder_get_position_deg(encoder_t *e)
+{
+    __encoder_update(e);
+    return (float) cur_cnt / ((float) (4 * e -> ppr * e -> resolution)) * 360.0;
+}
+
+float encoder_get_position_rad(encoder_t *e)
+{
+    __encoder_update(e);
+    return (float) cur_cnt / ((float) (4 * e -> ppr * e -> resolution)) * M_PI / 180.0;
+}
