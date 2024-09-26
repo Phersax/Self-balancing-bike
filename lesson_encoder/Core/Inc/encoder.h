24170167e16060ed891640da9802b8fafb94295a
simo1101 <152496154+simo1101@users.noreply.github.com>
2024-09-26 21:59:31 +0200

    Update encoder.h

--- lesson_encoder/Core/Inc/encoder.h
+++ lesson_encoder/Core/Inc/encoder.h
@@ -4,20 +4,21 @@
 #include "main.h"
 
 typedef enum {
-    ENCODER_RES_2 = 2,
-    ENCODER_RES_4 = 4
-} encoder_resolution_t;
+    A = TIM_CHANNEL_1,
+    B = TIM_CHANNEL_2,
+	AB = TIM_CHANNEL_ALL
+} channel;
 
 typedef struct {
     TIM_HandleTypeDef *tim;
     uint32_t last_sampling_t;
     uint32_t last_count;
     uint32_t ppr;
-    encoder_resolution_t resolution;
-    float velocity_pps;
+    int resolution;
+    uint32_t velocity_pps;
 } encoder_t;
 
-HAL_StatusTypeDef encoder_init(encoder_t *e, encoder_resolution_t resolution, TIM_HandleTypeDef *htim, uint32_t ppr);
+HAL_StatusTypeDef encoder_init(encoder_t *e, channel ch, TIM_HandleTypeDef *htim, uint32_t ppr);
 
 float encoder_get_velocity_rps(encoder_t *encoder);
 float encoder_get_velocity_rpm(encoder_t *encoder);
